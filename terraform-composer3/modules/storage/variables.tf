3579aed7767fcc55dcf33af5a4a55255d372659a
Amritha Devadiga <amritha.devadiga@fractal.ai>
2026-03-05 20:33:53 +0530

    Force add dev tfvars

--- terraform-composer3/modules/storage/variables.tf
+++ terraform-composer3/modules/storage/variables.tf
@@ -1,5 +1,21 @@
-variable "project_id"   { type = string }
-variable "location"     { type = string }
-variable "create"       { type = bool   default = false }
-variable "bucket_name"  { type = string }
-variable "force_destroy" { type = bool  default = false }
+variable "project_id" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "create" {
+  type    = bool
+  default = false
+}
+
+variable "bucket_name" {
+  type = string
+}
+
+variable "force_destroy" {
+  type    = bool
+  default = false
+}
