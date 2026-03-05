6a44cd3d24c0825784be7beb3234c0eb4a9117a7
Amritha Devadiga <amritha.devadiga@fractal.ai>
2026-03-05 20:38:12 +0530

    updated variables in output.tf

--- terraform-composer3/modules/iam/variables.tf
+++ terraform-composer3/modules/iam/variables.tf
@@ -1,4 +1,15 @@
-variable "project_id"            { type = string }
-variable "service_account_email" { type = string }
-variable "project_roles"         { type = list(string) }
-variable "bucket_name"           { type = string }
+variable "project_id" {
+  type = string
+}
+
+variable "service_account_email" {
+  type = string
+}
+
+variable "project_roles" {
+  type = list(string)
+}
+
+variable "bucket_name" {
+  type = string
+}
--- terraform-composer3/variables.tf
+++ terraform-composer3/variables.tf
@@ -1,37 +1,82 @@
-variable "project_id" { type = string }
-variable "region"     { type = string  default = "us-central1" }
+variable "project_id" {
+  type = string
+}
 
-# Existing infra (use self-links)
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+# Existing networking (Private IP)
 variable "network_self_link" {
   type        = string
-  description = "Existing VPC self link"
+  description = "Self link of existing VPC network"
   default     = "projects/prj-aidataandanalytics-s-24362/global/networks/dmi-network"
 }
+
 variable "subnet_self_link" {
   type        = string
-  description = "Existing Subnet self link"
+  description = "Self link of existing subnetwork"
   default     = "projects/prj-aidataandanalytics-s-24362/regions/us-central1/subnetworks/dmi-network"
 }
 
-variable "composer_env_name"      { type = string  default = "airflow-poc-dev" }
-variable "composer_image_version" { type = string  default = "composer-3-airflow-2.10.5-build.27" }
-variable "service_account_email"  { type = string  default = "composer-dag-deployer@prj-aidataandanalytics-s-24362.iam.gserviceaccount.com" }
-variable "dags_bucket"            { type = string  default = "composer_dags_dev" }
-variable "allowed_cidr"           { type = string  default = "0.0.0.0/0" }
+# Composer env basics
+variable "composer_env_name" {
+  type    = string
+  default = "airflow-poc-dev"
+}
 
-variable "env_variables"            { type = map(string) default = {} }
-variable "pypi_packages"            { type = map(string) default = {} }
-variable "airflow_config_overrides" { type = map(string) default = {} }
-variable "web_server_plugins_enabled" { type = bool default = true }
+variable "composer_image_version" {
+  type    = string
+  default = "composer-3-airflow-2.10.5-build.27"
+}
 
+# Workload Service Account (used by Composer Pods)
+variable "service_account_email" {
+  type    = string
+  default = "composer-dag-deployer@prj-aidataandanalytics-s-24362.iam.gserviceaccount.com"
+}
+
+# Storage (existing bucket for DAGs)
+variable "dags_bucket" {
+  type        = string
+  description = "Existing GCS bucket name (no gs://)"
+  default     = "composer_dags_dev"
+}
+
+# Access & config
+variable "allowed_cidr" {
+  type    = string
+  default = "0.0.0.0/0"
+}
+
+variable "env_variables" {
+  type    = map(string)
+  default = {}
+}
+
+variable "pypi_packages" {
+  type    = map(string)
+  default = {}
+}
+
+variable "airflow_config_overrides" {
+  type    = map(string)
+  default = {}
+}
+
+variable "web_server_plugins_enabled" {
+  type    = bool
+  default = true
+}
+
+# IAM roles to grant to the Composer SA
 variable "composer_sa_project_roles" {
-  type        = list(string)
-  description = "Project-level roles for the Composer SA"
-  default     = [
+  type = list(string)
+  default = [
     "roles/composer.worker",
-    "roles/storage.objectAdmin",
-    "roles/artifactregistry.reader",
     "roles/logging.logWriter",
-    "roles/monitoring.metricWriter"
+    "roles/monitoring.metricWriter",
+    "roles/artifactregistry.reader"
   ]
 }
