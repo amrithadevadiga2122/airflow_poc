01b7b7cd615ee42308852721e2cb3a772b2cb1be
Amritha Devadiga <amritha.devadiga@fractal.ai>
2026-03-09 14:52:34 +0530

    updated bucket config

--- terraform-composer3/versions.tf
+++ terraform-composer3/versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~>= 5.30"
+      version = "~> 5.30"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~>= 5.30" 
+      version = "~> 5.30" 
     }
   }
 }
