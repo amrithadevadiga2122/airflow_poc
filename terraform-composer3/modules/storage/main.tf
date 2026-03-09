135cc488aab9cd2e428544bc9ff156e6906485ab
Amritha Devadiga <amritha.devadiga@fractal.ai>
2026-03-09 17:08:49 +0530

    updated bucket config

--- terraform-composer3/modules/storage/main.tf
+++ terraform-composer3/modules/storage/main.tf
@@ -13,6 +13,3 @@ resource "google_storage_bucket" "bucket" {
   # Optional: add labels or lifecycle rules here if you need them
 }
 
-output "bucket_name" {
-  value = var.create ? google_storage_bucket.bucket[0].name : var.bucket_name
-}
