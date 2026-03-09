9d04f5c382da2dcb64f3d971896a8d2452acea03
Amritha Devadiga <amritha.devadiga@fractal.ai>
2026-03-09 17:04:40 +0530

    updated bucket config

--- terraform-composer3/modules/storage/main.tf
+++ terraform-composer3/modules/storage/main.tf
@@ -1,7 +1,18 @@
 resource "google_storage_bucket" "bucket" {
   count         = var.create ? 1 : 0
+
   name          = var.bucket_name
   location      = var.location
   force_destroy = var.force_destroy
+
+  iam_configuration {
+    uniform_bucket_level_access = var.ubla_enabled
+    public_access_prevention    = var.public_access_prevention_enforced ? "enforced" : "unspecified"
+  }
+
+  # Optional: add labels or lifecycle rules here if you need them
 }
 
+output "bucket_name" {
+  value = var.create ? google_storage_bucket.bucket[0].name : var.bucket_name
+}
--- terraform-composer3/modules/storage/variables.tf
+++ terraform-composer3/modules/storage/variables.tf
@@ -19,3 +19,15 @@ variable "force_destroy" {
   type    = bool
   default = false
 }
+
+
+# New: security posture controls
+variable "ubla_enabled" {
+  type    = bool
+  default = true  # Uniform bucket‑level access must be enabled per org policy
+}
+
+variable "public_access_prevention_enforced" {
+  type    = bool
+  default = true  # Many orgs also enforce PAP; keep true unless your org allows otherwise
+}
